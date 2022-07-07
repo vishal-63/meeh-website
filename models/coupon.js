e9e3942934e051ea3ead7004090d6b3519c2af97
Vishal Shah <shahvishal910@gmail.com>
2022-07-07 18:36:55 +0530

    added refs in SchemaTypes

--- models/coupon.js
+++ models/coupon.js
@@ -1,30 +1,24 @@
 const mongoose = require("mongoose");
 
-const couponSchema=new mongoose.Schema({
-    coupon_code:{
-        type:String,
-        required:true,
-        minlength:4,
-    },
-    percentage:{
-        type:Number,
-        max:100,
-    },
-    max_discount:{
-        type:Number,
-    },
-    min_total:{
-        type:Number,
-    },
-    amount:{
-        type:Number,
-    },
-    is_deleted:{
-        type:Boolean,
-        default:false,
-    }
-})
+const couponSchema = new mongoose.Schema({
+  coupon_code: {
+    type: String,
+    required: true,
+    minlength: 4,
+  },
+  percentage: {
+    type: Number,
+    max: 100,
+  },
+  max_discount: Number,
+  min_total: Number,
+  amount: Number,
+  is_deleted: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-const Coupon = mongoose.model('coupon',couponSchema);
+const Coupon = mongoose.model("coupon", couponSchema);
 
 module.exports = Coupon;
