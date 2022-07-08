fa185a5e504cd280d7696d8e6f75799a2a51ca85
sidproj <morisidhraj001@gmail.com>
2022-07-08 16:09:38 +0530

    added register route and ejs, wishlist route and ejs product route and ejs about route in app and its ejs productdetails ejs

--- app.js
+++ app.js
@@ -8,21 +8,33 @@ dotenv.config();
 // Routes
 const loginRouter = require("./routes/login");
 const cartRouter = require("./routes/cart");
-const productRouter = require("./routes/productsRoutes");
+const productRouter = require("./routes/products");
+const wishlistRouter = require("./routes/wishlist");
+const registerRouter = require("./routes/register");
 
 // Models
 const User = require("./models/user");
 
+const app = express();
+
 // Ejs view engine
 app.set("view engine", "ejs");
+
+//declaring public directory to get assets from
+app.use(express.static(__dirname + "/public"));
 
 // Login route
 app.use("/login", loginRouter);
 app.use("/cart", cartRouter);
 app.use("/products", productRouter);
+app.use("/wishlist", wishlistRouter);
+app.use("/register", registerRouter);
 
-//declaring public directory to get assets from
-app.use(express.static(__dirname + "/public"));
+//about us route
+app.get("/about",(req,res)=>{
+  res.render('about');
+})
+
 
 mongoose
   .connect(process.env.MONGODBURI)
